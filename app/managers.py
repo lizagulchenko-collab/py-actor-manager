cc5b7828d9afb2272d70ae1ad828da77fd664964
Yelyzaveta Hulchenko <liza.gulchenko@gmail.com>
2026-09-08 14:03:28 +0300

    Fixed flake8

--- app/managers.py
+++ app/managers.py
@@ -4,13 +4,14 @@ from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self, db_name, table_name) -> None:
+    def __init__(self, db_name: str, table_name: str) -> None:
         self._connection = sqlite3.connect(db_name)
         self.table_name = table_name
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            "VALUES (?, ?)",
             (first_name, last_name,)
         )
         self._connection.commit()
